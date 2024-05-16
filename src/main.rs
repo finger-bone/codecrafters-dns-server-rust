93e064136be322d3cf7c2874b65fdb958a98e351
zend <zendwang040302@outlook.com>
2024-05-16 10:21:22 +0800

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -63,8 +63,27 @@ fn main() {
 
                 let request = Message::decode(&buf);
 
+                let response = Message {
+                    header: Header::builder()
+                        .qdcount(1)
+                        .unwrap()
+                        .ancount(1)
+                        .unwrap()
+                        .id(request.header.id)
+                        .unwrap()
+                        .opcode(request.header.opcode)
+                        .unwrap()
+                        .rd(request.header.rd)
+                        .unwrap()
+                        .rcode(if request.header.opcode == 0 { 0 } else { 4 })
+                        .unwrap()
+                        .build(),
+                    questions: vec![Question::builder().build()],
+                    answers: vec![Answer::builder().build()],
+                };
+
                 udp_socket
-                    .send_to(&build_response(request).encode(), source)
+                    .send_to(&response.encode(), source)
                     .expect("Failed to send response");
             }
             Err(e) => {
